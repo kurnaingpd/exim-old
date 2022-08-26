e3fa51c7517a9b758832c6eab681fd22e8182c5b
Kurnain Arsyi R <r.kurnain@gmail.com>
2022-08-26 15:03:57 +0700

    change config send mail

--- application/helpers/sendmails_helper.php
+++ application/helpers/sendmails_helper.php
@@ -5,13 +5,8 @@
     if(!function_exists('send_mails')) {
         function send_mails($subject, $to, $content)
         {
-            // require APPPATH.'vendor/phpmailer/phpmailer/src/Exception.php';
-            // require APPPATH.'vendor/phpmailer/phpmailer/src/PHPMailer.php';
-            // require APPPATH.'vendor/phpmailer/phpmailer/src/SMTP.php';
-
-            $mail = new PHPMailer();
-
             // SMTP configuration
+            $mail = new PHPMailer();
             $mail->isSMTP();
             $mail->Host     = 'smtp.office365.com';
             $mail->SMTPAuth = true;
@@ -22,12 +17,7 @@
             $mail->Timeout = 60;
             $mail->SMTPKeepAlive = true; 
             $mail->setFrom('no-reply@gonusa-distribusi.com', 'Auto Email GEXIM');
-            // $mail->addReplyTo('xxx@hostdomain.com', ''); //user email
-            // $mail->addAddress($to);
-            foreach($to as $receipt) {
-                $mail->addAddress($receipt);
-            }
-            
+            $mail->addAddress($to['email']);
             $mail->Subject = $subject;
             $mail->isHTML(true);
             $message = $content;
---
+++ assets/SignUpload/readme.txt
@@ -0,0 +1,1 @@
+Biar ada isinya
